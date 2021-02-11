0ca5b2770635e719e402070f3269aeb70f320431
p2acoo <78920246+p2acoo@users.noreply.github.com>
2021-02-11 19:23:21 +0100

    Add files via upload

---
+++ Rightprice.py
@@ -0,0 +1,14 @@
+rightprice = 750
+prix = int(input("Veuillez saisir le prix en 0 et 1000"))
+while prix > 1000:
+    continue
+while prix != rightprice:
+    if prix > rightprice:
+        print("Le juste prix est inférieur")
+        prix = int(input("Veuillez saisir le prix en 0 et 1000"))
+    else:
+        print("Le juste prix est supérieur")
+        prix = int(input("Veuillez saisir le prix en 0 et 1000"))
+
+
+print("Vous avez trouvé le juste prix qui est de {}".format(rightprice))
